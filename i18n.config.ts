715806a24eeaff4c18bebc1a1ff99afdf382e1b3
trongtindev <me@trongtin.dev>
2024-06-17 10:49:48 +0700

    chore: nuxt upgrade

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,11 +1,6 @@
 import en from './locales/en.json';
 import vi from './locales/vi.json';
 
-export const locales = [
-  { title: 'English', code: 'en', iso: 'en-US' },
-  { title: 'Vietnamese', code: 'vi', iso: 'vi-VN' }
-];
-
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'en',
