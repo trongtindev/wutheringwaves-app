b496aea9dc8c2ec1d421a0139225520e82571c32
Tin Nguyen Trong <me@trongtin.dev>
2024-07-05 20:27:48 +0700

    feat: add localization

--- getGacha.ps1
+++ getGacha.ps1
@@ -1,10 +1,43 @@
-# source: https://raw.githubusercontent.com/alpharmi/ww/main/getGacha.ps1
+# localization
+param (
+  [string]$locale = "en"
+)
+$localization = @{
+  "en" = @{
+    "findGamePath"          = "Attempting to find game path automatically..."
+    "enterGamePathManually" = "Couldn't automatically find game path. Please enter game path manually."
+    "invalidGamePath" = "Invalid game path. Please try again."
+    "unableToFindUrl" = "Unable to find Gacha Url. Please open Convene Records in game, or report here https://github.com/trongtindev/astrite-app/issues."
+    "gachaUrlResult" = "Gacha Url has been saved to clipboard."
+  }
+  "vi" = @{
+    "findGamePath"          = "Đang tự động tìm thư mục chứa Wuthering Waves..."
+    "enterGamePathManually" = "Không thể tìm thấy thư mục chứa Wuthering Waves. Vui lòng nhập đường dẫn tới Wuthering Waves thủ công."
+    "invalidGamePath" = "Thư mục chứa Wuthering Waves không đúng. Vui lòng thử lại."
+    "unableToFindUrl" = "Không tìm thấy Gacha URL. Vui lòng mở lịch sử trong game lên trước hoặc báo lỗi tại https://github.com/trongtindev/astrite-app/issues."
+    "gachaUrlResult" = "Gacha URL đã được sao chép vào bộ nhớ. Hoặc bạn có thể sao chép thủ công."
+  }
+}
+function Get-LocalizedString {
+  param (
+    [string]$key,
+    [string]$locale
+  )
+  if ($localization.ContainsKey($locale) -and $localization[$locale].ContainsKey($key)) {
+    return $localization[$locale][$key]
+  } else {
+    $defaultCulture = "en"
+    return $localization[$defaultCulture][$key]
+  }
+}
 
+# main application
 Add-Type -AssemblyName System.Web
 $ProgressPreference = 'SilentlyContinue'
 
-# Find Game
-Write-Output "Attempting to find game path automatically..."
+## Find Game
+$findGamePath = Get-LocalizedString -key "findGamePath" -locale $locale
+Write-Output $findGamePath
 
 $64 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
 $32 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
@@ -13,30 +46,35 @@ $gachaLogPathExists = Test-Path ($gamePath + '\Wuthering Waves Game\Client\Binar
 $method = "automatic"
 
 if (!$gamePath -or !$gachaLogPathExists) {
-    $method = "manual"
+  $method = "manual"
 
-    Write-Output " "
-    Write-Output "Couldn't automatically find game path. Please enter game path manually."
-    Write-Output "Ex. E:\Wuthering Waves"
-    Write-Output " "
+  Write-Output " "
+  $enterGamePathManually = Get-LocalizedString -key "enterGamePathManually" -locale $locale
+  Write-Output $enterGamePathManually
+  Write-Output "Ex. E:\Wuthering Waves"
+  Write-Output " "
 
-    $path = read-host "Game path: "
+  $path = read-host "Game path: "
+  $invalidGamePath = Get-LocalizedString -key "invalidGamePath" -locale $locale
 
-    if ($path) {
-        if ($path.EndsWith("Wuthering Waves")) {
-            $gamePath = $path
-            Write-Output "Manually found game path."
-        } elseif ($path.EndsWith("Wuthering Waves Game")) {
-            $gamePath = $path.replace("Wuthering Waves Game", "")
-            Write-Output "Manually found game path."
-        } else {
-            Write-Output "Invalid game path. Please try again."
-        }
-    } else {
-        Write-Output "Invalid game path. Please try again."
+  if ($path) {
+    if ($path.EndsWith("Wuthering Waves")) {
+      $gamePath = $path
+      Write-Output "Manually found game path."
     }
+    elseif ($path.EndsWith("Wuthering Waves Game")) {
+      $gamePath = $path.replace("Wuthering Waves Game", "")
+      Write-Output "Manually found game path."
+    }
+    else {
+      Write-Output $invalidGamePath
+    }
+  }
+  else {
+    Write-Output $invalidGamePath
+  }
 } else {
-    Write-Output "Automatically found game path."
+  Write-Output "Automatically found game path."
 }
 
 Write-Output " "
@@ -45,33 +83,38 @@ $gachaLogPath = $gamePath + '\Wuthering Waves Game\Client\Binaries\Win64\ThirdPa
 
 #Find Gacha Url
 if (Test-Path ($gachaLogPath + "\debug.log") -PathType Leaf) {
-    Write-Output "Finding Gacha Url..."
+  Copy-Item -Path ($gachaLogPath + "\debug.log") -Destination ($gachaLogPath + "\debug_copy.log")
+  $cacheData = Get-Content ($gachaLogPath + "\debug.log")
+  Remove-Item -Path ($gachaLogPath + "\debug_copy.log")
+  $cacheDataLines = $cacheData -split '1/0/'
+  $url = ""
 
-    Copy-Item -Path ($gachaLogPath + "\debug.log") -Destination ($gachaLogPath + "\debug_copy.log")
-    $cacheData = Get-Content ($gachaLogPath + "\debug.log")
-    Remove-Item -Path ($gachaLogPath + "\debug_copy.log")
-    $cacheDataLines = $cacheData -split '1/0/'
-    $url = ""
+  for ($i = $cacheDataLines.Length - 1; $i -ge 0; $i--) {
+    $line = $cacheDataLines[$i]
 
-    for ($i = $cacheDataLines.Length - 1; $i -ge 0; $i--) {
-        $line = $cacheDataLines[$i]
+    if ($line.Contains("https://aki-gm-resources-oversea.aki-game.net/aki/gacha/index.html#/record")) {
+      $url = (($line -split ': "')[1].replace('",', "")) + ("&wa_method=" + $method)
 
-        if ($line.Contains("https://aki-gm-resources-oversea.aki-game.net/aki/gacha/index.html#/record")) {
-            $url = (($line -split ': "')[1].replace('",', "")) + ("&wa_method=" + $method)
-
-            break
-        }
+      break
     }
+  }
 
-    if ($url) {
-        Write-Output " "
-        Write-Output $url
-        Set-Clipboard -Value $url
-        Write-Output " "
-        Write-Output "Gacha Url has been saved to clipboard."
-    } else {
-        Write-Output "Unable to find Gacha Url. Please open Convene Records in game."
-    }
+  $unableToFindUrl = Get-LocalizedString -key "unableToFindUrl" -locale $locale
+  if ($url) {
+    Write-Output " "
+    Write-Output $url
+    Set-Clipboard -Value $url
+    Write-Output " "
+
+    $gachaUrlResult = Get-LocalizedString -key "gachaUrlResult" -locale $locale
+    Write-Output $gachaUrlResult
+  } else {
+    Write-Output " "
+    Write-Output $unableToFindUrl
+    Write-Output " "
+  }
 } else {
-    Write-Output "Unable to find Gacha Url. Please open Convene Records in game."
+  Write-Output " "
+  Write-Output $unableToFindUrl
+  Write-Output " "
 }
