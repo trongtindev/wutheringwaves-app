3cdbdf812174d6a41273b0647dba8326f8a166f0
Tin Nguyen Trong <me@trongtin.dev>
2024-07-22 19:52:14 +0700

    chore: add auth redirect

--- composables/auth.ts
+++ composables/auth.ts
@@ -1,10 +1,13 @@
 import { defineStore } from 'pinia';
 import type { IUser } from '~/interfaces/user';
 import { jwtDecode } from 'jwt-decode';
+import { AxiosError } from 'axios';
 
 export const useAuth = defineStore('useAuth', () => {
   // uses
   const api = useApi();
+  const route = useRoute();
+  const router = useRouter();
   const sentry = useSentry();
   const runtimeConfig = useRuntimeConfig();
 
@@ -19,18 +22,35 @@ export const useAuth = defineStore('useAuth', () => {
 
   // functions
   const initialize = async () => {
+    if (route.query.code) {
+      state.value = 'sign-in';
+      signIn({ code: route.query.code.toString() })
+        .catch((error) => {
+          console.error(error);
+
+          // TODO: handle
+          if (error instanceof AxiosError) {
+            alert(error.response ? JSON.stringify(error.response.data) : error);
+          } else {
+            alert(error);
+          }
+        })
+        .finally(() => {
+          state.value = '';
+        });
+      return;
+    }
+
     if (isSignedIn.value) {
       refreshProfile();
+      return;
     }
-    console.log('gState', gState.value);
   };
 
-  const signIn = async (options?: { useFedCM?: boolean }) => {
+  const signIn = async (options?: { useFedCM?: boolean; code?: string }) => {
     options ??= {};
-    /**
-     * Chrome third-party cookie deprecation starts Q1 2024.
-     * https://developers.google.com/identity/gsi/web/guides/fedcm-migration
-     */
+    // TODO: Chrome third-party cookie deprecation starts Q1 2024.
+    // https://developers.google.com/identity/gsi/web/guides/fedcm-migration
     options.useFedCM ??= device.isMobile;
 
     gState.value = null;
@@ -47,37 +67,51 @@ export const useAuth = defineStore('useAuth', () => {
       isScriptLoaded.value = true;
     }
 
-    console.debug('google.accounts.id.initialize');
-    const credential = await new Promise<string | null>((resolve, reject) => {
-      window.google.accounts.id.initialize({
-        client_id: runtimeConfig.public.GOOGLE_CLIENT_ID,
-        context: 'signin',
-        callback: (result) => {
-          console.debug('google.accounts.id.initialize', 'callback', result);
-          resolve(result.credential);
-        },
-        auto_select: true,
-        cancel_on_tap_outside: false,
-        use_fedcm_for_prompt: options.useFedCM
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const payload: any = {
+      code: options.code || undefined,
+      idToken: undefined
+    };
+    if (!options.code) {
+      console.debug('google.accounts.id.initialize');
+      const credential = await new Promise((resolve) => {
+        window.google.accounts.id.initialize({
+          client_id: runtimeConfig.public.GOOGLE_CLIENT_ID,
+          context: 'signin',
+          callback: (result) => {
+            console.debug('google.accounts.id.initialize', 'callback', result);
+            resolve(result.credential);
+          },
+          auto_select: true,
+          cancel_on_tap_outside: false,
+          use_fedcm_for_prompt: options.useFedCM
+        });
+        window.google.accounts.id.prompt(async (result) => {
+          console.debug('google.accounts.id.prompt', result);
+          if (result.isNotDisplayed()) {
+            resolve('redirect');
+          } else if (result.isSkippedMoment()) {
+            resolve(null);
+          }
+        });
       });
-      window.google.accounts.id.prompt(async (result) => {
-        console.debug('google.accounts.id.prompt', result);
-        if (result.isNotDisplayed()) {
-          reject('isNotDisplayed');
-        } else if (result.isSkippedMoment()) {
-          resolve(null);
-        }
+      console.debug('google.accounts.id.initialize', credential);
+      if (!credential) return;
+      if (credential === 'redirect') {
+        window.location.href = `${runtimeConfig.public.API_URL}/auth/redirect`;
+        return;
+      }
+      payload.idToken = credential;
+    } else {
+      router.replace({
+        path: route.path
       });
-    });
-    console.debug('google.accounts.id.initialize', credential);
-    if (!credential) return;
+    }
 
     const signInResult = await api.getInstance().post<{
       accessToken: string;
       refreshToken: string;
-    }>('auth/signin', {
-      credential
-    });
+    }>('auth/signin', payload);
     accessToken.value = signInResult.data.accessToken;
     refreshToken.value = signInResult.data.refreshToken;
 
