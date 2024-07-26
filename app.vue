f913dfbc0baa9fe2a545aaaa0750ee586a500113
Tin Nguyen Trong <me@trongtin.dev>
2024-07-26 23:00:31 +0700

    chore: add googe font

--- app.vue
+++ app.vue
@@ -55,6 +55,19 @@ useHead({
       type: 'application/rss+xml',
       href: `${SITE_URL}/guides/rss`
     },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossorigin: true
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
+    },
     ...(i18nHead.value.link || []).map((e) => {
       return {
         ...e,
