800bd9a04ee5151c960bf4bbb2844e1efa47308a
Tin Nguyen Trong <me@trongtin.dev>
2024-07-26 22:59:53 +0700

    chore: fix rss path

--- server/api/rss/guides.ts
+++ server/api/rss/guides.ts
@@ -1,18 +1,23 @@
 import RSS from 'rss';
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import type { IPost } from '~/interfaces/post';
+import type { IListResponse } from '~/interfaces/api';
 
 export default defineEventHandler(async (event) => {
   const { SITE_URL, API_URL } = useRuntimeConfig().public;
+  const request = axios.create({
+    baseURL: API_URL
+  });
+  axiosRetry(request);
 
   const feed = new RSS({
     title: 'WutheringWaves.app',
     site_url: SITE_URL,
-    feed_url: `${SITE_URL}/api/rss`
+    feed_url: `${SITE_URL}/guides/rss`
   });
-  const result = await axios.get<{
-    items: IPost[];
-  }>(`${API_URL}/posts`);
+
+  const result = await request.get<IListResponse<IPost>>('posts');
 
   for (const item of result.data.items) {
     feed.item({
@@ -28,4 +33,3 @@ export default defineEventHandler(async (event) => {
   event.node.res.setHeader('content-type', 'text/xml');
   event.node.res.end(feedString);
 });
-
